2d6cbdce356a4f41a94b824af97fb8048915c396
donCESAR12345 <sprit152009@gmail.com>
2022-08-09 21:21:02 -0500

    Refactored functions to not repeat myself, added past date restriction
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

--- Seguimiento5/js/script.js
+++ Seguimiento5/js/script.js
@@ -5,52 +5,74 @@ window.onload = function()
   // Aplicar la debida restricción a cada entrada de información
   for(let i = 0; i < data.length; i++)
   {
+    let data_element = data[i];
     // Restringir entradas no numéricas
-    if(data[i].classList.contains("no-numbers"))
+    if(data_element.classList.contains("no-numbers"))
     {
-      data[i].onkeypress = restrict_no_numbers;
+      data_element.onkeypress = restrict_no_numbers;
     }
     // Restringir entradas solamente numéricas
-    else if(data[i].classList.contains("only-numbers"))
+    else if(data_element.classList.contains("only-numbers"))
     { 
-      data[i].onkeypress = restrict_only_numbers;
+      data_element.onkeypress = restrict_only_numbers;
     }
-    // Validar fechas futuras
-    else if(data[i].type == "date")
+    // Restringir entradas de direcciones
+    else if(data_element.classList.contains("address"))
     {
-      data[i].onchange = validate_date;
+      data_element.onkeypress = restrict_address;
+    }
+    // Restringir fechas futuras
+    else if(data_element.classList.contains("date"))
+    {
+      restrict_date(data_element);
     }
   }
+}
+
+function basic_restrict(regex, key, value)
+{
+  // Se valida el char code ingresado
+  let char_code = (typeof key.which == "undefined") ? key.keyCode : key.which;
+  // Se transforma el caracter tecleado en string
+  let str_char_code = String.fromCharCode(char_code);
+  // Se verifica si es doble espacio
+  let double_space_ok = value[value.length - 1] != ' ' || str_char_code != ' ';
+  // Se actualiza el valor para comprobar la regex
+  let value_to_check = value + str_char_code;
+
+  return(regex.test(value_to_check) && double_space_ok);
 }
 
 function restrict_no_numbers(event)
 {
   // Se declara la regex de nombres permitidos
   const valid_regex = /^(?! +)[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]+$/i;
-  // Se valida el char code ingresado
-  let char_code = (typeof event.which == "undefined") ? event.keyCode : event.which;
-  // Se concatena el valor recién ingresado con el valor anterior
-  let actual_value = this.value + String.fromCharCode(char_code);
-
-  return(valid_regex.test(actual_value));
+  return(basic_restrict(valid_regex, event, this.value));
 }
 
 function restrict_only_numbers(event)
 {
   // Se declara la regex de solamente números sin espacios al inicio 
   const valid_regex = /^(?! +)[\d]+$/i;
-  // Se valida el char code ingresado
-  let char_code = (typeof event.which == "undefined") ? event.keyCode : event.which;
-  // Se concatena el valor recién ingresado con el valor anterior
-  let actual_value = this.value + String.fromCharCode(char_code);
-
-  return(valid_regex.test(actual_value));
+  return(basic_restrict(valid_regex, event, this.value));
 }
 
-// TODO: FIX THIS, IT'S NOT WORKING
-function validate_date()
+function restrict_address(event)
 {
-  const today = new Date();
-  input_date = new Date(this.value);
-  return (today > input_date);
+  // Se declara la regex de caracteres válidos de las direcciones
+  const valid_regex = /^(?! +)[\d\w#\- ]+$/i;
+  return(basic_restrict(valid_regex, event, this.value));
 }
+
+function restrict_date(date_element)
+{
+  // Obtiene la fecha de hoy
+  let today = new Date();
+  // Le resta un día
+  today.setDate(today.getDate() - 1);
+  // Toma el substring de la fecha en formato deseado
+  today = today.toISOString().slice(0, 10);
+  // Declara el máximo del elemento
+  date_element.max = today;
+}
+
