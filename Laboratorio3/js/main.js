8710a4330f8a633d059367167b8e077e9efeb23e
donCESAR12345 <sprit152009@gmail.com>
2022-09-04 17:43:48 -0500

    Finished HTML and CSS for calculator
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

---
+++ Laboratorio3/js/main.js
@@ -0,0 +1,27 @@
+function sum(a, b)
+{
+  return a + b;
+}
+
+function minus(a, b)
+{
+  return a - b;
+}
+
+function multiplication(a, b)
+{
+  return a * b;
+}
+class function_button
+{
+  constructor(object, funct)
+  {
+    this.object = object;
+    this.funct = funct;
+  }
+}
+
+window.onload = function()
+{
+   
+}
